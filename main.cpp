de692da970bab8a4a38563a4b56cb18c01dd2c36
EdoardoMaestri <115471996+EdoardoMaestri@users.noreply.github.com>
2022-11-17 18:38:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
+   float a,b;
+   int o;
+   cin >> a >> b >> o;
+   switch (o)
+   {
+        case 0:
+            cout << (a*b)/2;
+            break;
+        case 1:
+            cout << a*a;
+            break;
+        case 2:
+            cout << a*b;
+            break;
+        default:
+            cout << "opzione non valida";
+   }
    return 0;
 }
